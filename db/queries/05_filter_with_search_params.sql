2a4a8cdf7c1b54eac5b59978f42e58b2932192e7
ayan-hassan <ahassan@hotmail.ca>
2023-03-16 17:23:03 -0400

    join users to resources queries

--- db/queries/05_filter_with_search_params.sql
+++ db/queries/05_filter_with_search_params.sql
@@ -1,7 +1,10 @@
-SELECT resources.id, resources.title, resources.user_id, resources.category_id, count(liked_resources.resource_id) as number_of_likes, round(avg(resource_ratings.rating),2) as average_rating
+SELECT resources.id, resources.title, resources.user_id, users.username as username, users.profile_pic, resources.category_id, count(liked_resources.resource_id) as number_of_likes, round(avg(resource_ratings.rating),2) as average_rating
 FROM resources
 LEFT OUTER JOIN liked_resources ON liked_resources.resource_id = resources.id
 LEFT OUTER JOIN resource_ratings ON resource_ratings.resource_id = resources.id
+LEFT OUTER JOIN users ON resources.user_id = users.id
+
+
 
 
 WHERE resources.category_id = 1
@@ -9,7 +12,7 @@ WHERE resources.category_id = 1
   -- AND (liked_resources.user_id = 2 OR resources.user_id = 2)
 
 
-GROUP BY resources.id
+GROUP BY resources.id, users.username, users.profile_pic,
 
 HAVING avg(resource_ratings.rating) >= 4
 
