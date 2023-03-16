32f228561b1408dd5f72ff3a855b0f363ce5d27d
ayan-hassan <ahassan@hotmail.ca>
2023-03-16 18:35:18 -0400

    fix 01 query

--- db/queries/01_fetch_all_resources.sql
+++ db/queries/01_fetch_all_resources.sql
@@ -1,7 +1,22 @@
-SELECT resources.*, count(liked_resources.resource_id) as number_of_likes, round(avg(resource_ratings.rating),2) as average_rating
+SELECT resources.id, count(liked_resources.resource_id) as number_of_likes, round(avg(resource_ratings.rating),2) as average_rating
 FROM resources
 LEFT OUTER JOIN liked_resources ON liked_resources.resource_id = resources.id
 LEFT OUTER JOIN resource_ratings ON resource_ratings.resource_id = resources.id
 GROUP BY resources.id
-ORDER BY number_of_likes DESC
+ORDER BY resources.id
+;
+
+SELECT resources.id, count(liked_resources.resource_id) as number_of_likes
+FROM resources
+LEFT OUTER JOIN liked_resources ON liked_resources.resource_id = resources.id
+GROUP BY resources.id
+ORDER BY resources.id
+;
+
+SELECT resources.id, count(liked_resources.resource_id) as number_of_likes, round(avg(resource_ratings.rating),2) as average_rating
+FROM resources
+LEFT OUTER JOIN resource_ratings ON resource_ratings.resource_id = resources.id
+LEFT OUTER JOIN liked_resources ON liked_resources.resource_id = resources.id
+GROUP BY resources.id
+ORDER BY resources.id
 ;
